1c5be9a6698681cc093bd489ba01f1abfab8b2e2
Mounika Kella <45024975+mounikakella@users.noreply.github.com>
2026-06-08 16:22:56 +0530

    Create lilly.py

---
+++ lilly.py
@@ -0,0 +1,62 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+# You have a long string of text.
+# Write a function chunkText(text, chunkSize, overlap) that:
+ 
+# - Splits the text into chunks of chunkSize characters
+# - Each consecutive chunk overlaps by overlap characters
+#   with the previous one
+# - Returns an array/list of chunk strings
+
+# def chunkText(text, chunkSize, overlap):
+#     res = []
+#     i=0
+#     while i < len(text):
+#         res.append(text[i:i+chunkSize])
+#         i += chunkSize - overlap
+#     return res
+    
+# print(chunkText('Write Python 3 code in this online editor and run it.', 4, 2))
+
+You have a list of 20 IDs. 
+Write a function that calls this dummy function for ALL 20 IDs with a maximum of 5 concurrent requests at any time. 
+Results must be returned in the SAME ORDER as the input list.
+
+xyz.com/1
+xyz.com/2
+start = 0
+end = 5
+while id <=20
+    for n in range(start, end):
+        promise
+
+
+# 1-5, 6-10,
+import asyncio
+from collections import OrderedDict
+
+start = 1
+end = 5
+
+while end <=20:
+    
+    for n in range(start, end):
+        n = 1
+        calls = calls.append(api_call(n))
+    asyncio.gather(*calls)
+    class =  []
+    start += 5
+    end += 5
+
+def api_call(n):
+    return n
+
+
+0-5
+6-10
+1
+
+why nestjs next why not python 
+
+
